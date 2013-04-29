2c92af7db662a2576f97e0ece538bb75494cda77
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2013-04-29 12:05:07 +0100

    Check in of Custom Types example
    * Note PSM sample is waiting on #1668 to work fully due to effect class breaks

---
+++ CustomModelSample/CustomModelClassSample.WindowsPhone/Game1.cs
@@ -0,0 +1,125 @@
+using CustomModelTypes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CustomModelClassSample.WindowsPhone
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+
+        CustomModel model;
+
+        Matrix world;
+        Matrix view;
+        Matrix projection;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            model = Content.Load<CustomModel>("tank");
+
+            // Calculate camera view and projection matrices.
+            view = Matrix.CreateLookAt(new Vector3(1000, 500, 0),
+                                       new Vector3(0, 150, 0), Vector3.Up);
+
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4,
+                                                             GraphicsDevice.Viewport.AspectRatio, 10, 10000);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+            HandleInput();
+
+            // Update the world transform to make the model rotate.
+            float time = (float)gameTime.TotalGameTime.TotalSeconds;
+
+            world = Matrix.CreateRotationY(time * 0.1f);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            model.Draw(world, view, projection);
+
+            base.Draw(gameTime);
+        }
+
+        #region Handle Input
+
+
+        /// <summary>
+/// Handles input for quitting the game.
+        /// </summary>
+        private void HandleInput()
+        {
+            KeyboardState currentKeyboardState = Keyboard.GetState();
+            GamePadState currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+            // Check for exit.
+            if (currentKeyboardState.IsKeyDown(Keys.Escape) ||
+                currentGamePadState.Buttons.Back == ButtonState.Pressed)
+            {
+                Exit();
+            }
+        }
+
+
+        #endregion
+    }
+}
